9e62f3367e4ec8d73b56e873ca6bd7358280c9e4
Ari Fikri <arirushan79@gmail.com>
2025-12-03 18:26:15 +0700

    change vite config for github deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/finance-dashboard/' 
-})
+  base: command === "build" ? "/finance-dashboard/" : "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    }
+  }
+}))
